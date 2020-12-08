b483a6e943ea431510dd02a9ea63162c2da98cd4
Marcos Paulo <marcosp.mail@gmail.com>
2020-12-08 15:50:27 -0300

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('marcospmail/docker-nodejs-with-dsl-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
